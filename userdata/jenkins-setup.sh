169d96e4f88c32372cbece68726450c7a01b882b
Arvind Jaiswal <26666567+arvindjai@users.noreply.github.com>
2025-12-03 18:17:07 +0530

    Update Jenkins setup script for OpenJDK version

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
  
-sudo apt install openjdk-17-jdk -y
+sudo apt install fontconfig openjdk-21-jre -y
  
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
@@ -9,7 +9,7 @@ https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
 https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
 /etc/apt/sources.list.d/jenkins.list > /dev/null
- 
+
 sudo apt-get update
  
 sudo apt-get install jenkins -y
